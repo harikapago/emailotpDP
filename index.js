43c57253292a369224f24daa654959eb6a7ad27a
harikapago <146166025+harikapago@users.noreply.github.com>
2023-10-17 17:30:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const nodemailer = require('nodemailer');
 
+// just
+
 const app = express();
 app.use(express.json());
 app.use(cors());
